93450d3763e642b4c2a040acfe18402aefff6830
Phisinee Sagulvanitchaporn <phisinee.s@pomelofashion.com>
2021-01-22 14:36:40 +0700

    test-4

--- pomelo/aws/ap-southeast-1/ec2.tf
+++ pomelo/aws/ap-southeast-1/ec2.tf
@@ -30,9 +30,16 @@ resource "aws_security_group" "ubuntu" {
 
   ingress {
     description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
