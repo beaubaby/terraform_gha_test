adc6adfa10b173f61eaed2217bbcdd91efe4343b
Phisinee Sagulvanitchaporn <phisinee.s@pomelofashion.com>
2021-01-22 14:24:38 +0700

    test

--- pomelo/aws/ap-southeast-1/ec2.tf
+++ pomelo/aws/ap-southeast-1/ec2.tf
@@ -36,13 +36,6 @@ resource "aws_security_group" "ubuntu" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "terraform"
   }
